54e48d387fdf9efd10f67ee865b1395cf623c7e4
r-neil <nreisenauer@gmail.com>
2016-09-01 10:58:00 -0400

    added unit tests

--- directtrains/tests.py
+++ directtrains/tests.py
@@ -13,17 +13,27 @@ class GetRequestTest(unittest.TestCase):
 		train_line = models.TrainLine.objects.all()
 
 		models.DirectTrain.objects.create(train_line=train_line[0], train_num='123', effective_date=date(2016,8,31))
+		models.DirectTrain.objects.create(train_line=train_line[0], train_num='456', effective_date=date(2016,8,31))
+		models.DirectTrain.objects.create(train_line=train_line[0], train_num='789', effective_date=date(2016,8,31))
+		models.DirectTrain.objects.create(train_line=train_line[0], train_num='1234', effective_date=date(2016,8,31))
+		models.DirectTrain.objects.create(train_line=train_line[0], train_num='5678', effective_date=date(2016,8,31))
+		models.DirectTrain.objects.create(train_line=train_line[0], train_num='4567', effective_date=date(2016,8,31))
+		
+		self.factory = APIRequestFactory()
+		self.view = views.ListDirectTrains.as_view()
+		self.request = self.factory.get('')
 
-	def test_new_app_better(self):
-		factory = APIRequestFactory()
-		view = views.ListDirectTrains.as_view()
-		request = factory.get('')
-		response = view(request, "1983","11","09")
-		response.render()
-		print("count is: {}".format(response.content.count))
-		print("data is: {}".format(response.content))
-		assert len(response.content) > 0
+	def tearDown(self):
+		models.TrainLine.objects.all().delete()
+		models.DirectTrain.objects.all().delete()
+
+	def test_return_train_info(self):
+		response = self.view(self.request, "1983","11","09")
+		assert len(response.data) == models.DirectTrain.objects.count()
+
+	def test_return_no_train_info(self):
+		response = self.view(self.request, '2016','9','1')
+		assert len(response.data) == 0
 
 if __name__ == '__main__':
-	settings.configure()
 	unittest.main()
